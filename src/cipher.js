d5a7979cdfbec355d1c93fdb5865946cde47f4ec
unknown <natalypc27@gmail.com>
2018-05-29 22:05:46 -0500

    se subieron cambios en los archivos cipher,index y style

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,60 @@
 window.cipher = {
   // ... 
 };
+window.cipher = {
+  // La funcion cifra la palabra la inresar
+ encode: function (offset,string){//se ingresa dos parametrosla cantidad de desplazamiento y los datos de la caja
+  //se declaran los ocntenedores
+  let caracterA = "";
+  let caracter = " ";
+  let encryptWord= "";
+  let desplazarN= parseInt(offset);//parsea mi dato y me devuelve un entero
+//Se re crea un bucle para recorrer toda la palabra o frase ingresada
+ for (let i = 0; i < string.length; i++) {
+  let codigoAs = string.charCodeAt(i);// se declara la variable codigoAd donde se almacenara el codigo ASCII de la primera letra de la palabra
+    if (codigoAs >= 65 && codigoAs<=90) {//codicionamos delimitando el caracter que son MAYUSCULAS
+       caracterA = ((codigoAs-65+desplazarN)%26 + 65); //formula para realizar el desplazamiento fijo que se da, obteniendo el nuevo caracter
+    }else if(codigoAs >= 97 && codigoAs<=122){//codicionamos delimitando el caracter que son minusculas
+            caracterA = ((codigoAs-97+desplazarN)%26 + 97); //formula para realizar el desplazamiento fijo que se da, obteniendo el nuevo caracter
+          }else{
+            caracterA = string.charAt(i);//se obtiene el mismo valor
+          }
+    caracter = String.fromCharCode(caracterA);// registra el cambio del codigo ASCII a la palabra correcta
+    encryptWord += caracter;//el contenedor almacena cada caracter
+  }
+ return encryptWord;// Aqui llamo a la funcion que hara el desplazamiento
+},
+decode: function (offset,string){
+
+  let caracterAc = "";
+  let caracterD = "";
+  let desencryptWord= "";
+  let desplazarN= parseInt(offset);//parsea mi dato y me devuelve un entero
+  
+ for (let j = 0; j < string.length; j++) {
+
+  let codigoAsc = string.charCodeAt(j);
+
+    if (codigoAsc >= 65 && codigoAsc<=90) {
+
+       caracterAc = ((codigoAsc-90-desplazarN)%26 + 90); 
+
+     }else if(codigoAsc >= 97 && codigoAsc<=122){
+
+     caracterAc = ((codigoAsc-122-desplazarN)%26 + 122); 
+      }
+    caracterD = String.fromCharCode(caracterAc);
+    desencryptWord += caracterD;
+  }
+  return desencryptWord;
+},
+createCipherWithOffset: function (){
+  const string= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const encodedS= cipher.encode(string);
+
+mensajeS.value(encodedS);
+  const decodedString=cipher.decode(string);
+  console.log(decodedString);
+}
+
+};
---
+++ src/index.js
@@ -0,0 +1,16 @@
+// se declara variables para capturar los ids del html y los datos que se incluyan
+let mensajeE = document.getElementById("textI");
+let mensajeS= document.getElementById("textO"); // se declara la vaariable donde se obtendra el contenido donde se ingreso el id
+let numb=document.getElementById("camb");
+//se declara las variables para capturar los ids de los buttons 
+let btnC= document.getElementById("cipherB");
+let btnD= document.getElementById("decipherB");
+
+//se crean dos eventos button donde se pasan las funciones del cifrado y descifrado, 
+btnC.addEventListener('click',function(){
+mensajeS.value=cipher.encode(numb.value,mensajeE.value);
+});
+ 
+btnD.addEventListener('click',function(){
+  mensajeS.value=cipher.decode(numb.value,mensajeE.value);
+});
