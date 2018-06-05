74c113d04ba293fbc5c47aaebb79a7aa98c49742
NatalyPorras <natalypc27@gmail.com>
2018-06-05 18:39:44 -0500

    subiendo cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,6 @@
 window.cipher = {
   // La funcion cifra la palabra la inresar
- encode: (offset,string) =>{//se ingresa dos parametrosla cantidad de desplazamiento y los datos de la caja
+ encode: (offset,string) =>  {//se ingresa dos parametrosla cantidad de desplazamiento y los datos de la caja
   //se declaran las variables que van a contener ciertas valores
   let caracterA = "";
   let caracter = " ";
@@ -11,7 +11,7 @@ window.cipher = {
   let codigoAs = string.charCodeAt(i);// se declara la variable codigoAd donde se almacenara el codigo ASCII de la primera letra de la palabra
     if (codigoAs >= 65 && codigoAs<=90) {//codicionamos delimitando el caracter que son MAYUSCULAS
        caracterA = ((codigoAs-65+offset)%26 + 65); //formula para realizar el desplazamiento fijo que se da, obteniendo el nuevo caracter
-    }else if(codigoAs >= 97 && codigoAs<=122){//codicionamos delimitando el caracter que son minusculas
+    }else if(codigoAs >= 97 && codigoAs<=122)  {//codicionamos delimitando el caracter que son minusculas
             caracterA = ((codigoAs-97+offset)%26 + 97); //formula para realizar el desplazamiento fijo que se da, obteniendo el nuevo caracter
           }else{
             caracterA = codigoAs;//se obtiene el mismo valor
@@ -22,7 +22,7 @@ window.cipher = {
  return encryptWord;// Aqui llamo a la funcion que hara el desplazamiento
 },
   // La funcion cifra la palabra la inresar
-decode: (offset,string)=>{
+decode: (offset,string)=>  {
   //se declaran las variables que van a contener ciertas valores
   let caracterAc = "";
   let caracterD = "";
@@ -33,7 +33,7 @@ decode: (offset,string)=>{
   let codigoAsc = string.charCodeAt(j);// se declara la variable codigoAd donde se almacenara el codigo ASCII de la primera letra de la palabra
     if (codigoAsc >= 65 && codigoAsc<=90) {//codicionamos delimitando el caracter que son MAYUSCULAS
        caracterAc = ((codigoAsc-90-offset)%26 + 90); //formula para realizar el desplazamiento fijo que se da, obteniendo el nuevo caracter
-    }else if(codigoAsc >= 97 && codigoAsc<=122){//codicionamos delimitando el caracter que son minusculas
+    }else if(codigoAsc >= 97 && codigoAsc<=122)  {//codicionamos delimitando el caracter que son minusculas
            caracterAc = ((codigoAsc-122-offset)%26 + 122); //formula para realizar el desplazamiento fijo que se da, obteniendo el nuevo caracter
           }else{
             caracterAc = codigoAsc;//se obtiene el mismo valor
@@ -43,12 +43,12 @@ decode: (offset,string)=>{
   }
   return desencryptWord;
 },
-createCipherWithOffset: (offset)=>{
+createCipherWithOffset: (offset)=>  {
 let cipherED = {
-  encode:(string)=>{
+  encode:(string)=>  {
     return cipher.encode(offset,string);
   },
-  decode:(string)=>{
+  decode:(string)=>  {
     return cipher.decode(offset,string);
   }
 };
