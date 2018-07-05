9fad4ca8fd082aa0f7c43459d6c6e2bab6d3e671
unknown <natalypc27@gmail.com>
2018-07-05 09:28:59 -0500

    subiendo cambios de issues

--- src/cipher.js
+++ src/cipher.js
@@ -1,57 +1,57 @@
 window.cipher = {
   // La funcion cifra la palabra la inresar
- encode: (offset,string) =>  {//se ingresa dos parametrosla cantidad de desplazamiento y los datos de la caja
-  //se declaran las variables que van a contener ciertas valores
-  let caracterA = "";
-  let caracter = " ";
-  let encryptWord= "";
- // let desplazarN= parseInt(offset);//parsea mi dato y me devuelve un entero
-//Se re crea un bucle para recorrer toda la palabra o frase ingresada
- for (let i = 0; i < string.length; i++) {
-  let codigoAs = string.charCodeAt(i);// se declara la variable codigoAd donde se almacenara el codigo ASCII de la primera letra de la palabra
-    if (codigoAs >= 65 && codigoAs<=90) {//codicionamos delimitando el caracter que son MAYUSCULAS
-       caracterA = ((codigoAs-65+offset)%26 + 65); //formula para realizar el desplazamiento fijo que se da, obteniendo el nuevo caracter
-    }else if(codigoAs >= 97 && codigoAs<=122)  {//codicionamos delimitando el caracter que son minusculas
-            caracterA = ((codigoAs-97+offset)%26 + 97); //formula para realizar el desplazamiento fijo que se da, obteniendo el nuevo caracter
-          }else{
-            caracterA = codigoAs;//se obtiene el mismo valor
-          }
-    caracter = String.fromCharCode(caracterA);// registra el cambio del codigo ASCII a la palabra correcta
-    encryptWord += caracter;//el contenedor almacena cada caracter
-  }
- return encryptWord;// Aqui llamo a la funcion que hara el desplazamiento
-},
-  // La funcion cifra la palabra la inresar
-decode: (offset,string)=>  {
-  //se declaran las variables que van a contener ciertas valores
-  let caracterAc = "";
-  let caracterD = "";
-  let desencryptWord= "";
- // let desplazarN= parseInt(offset);//parsea mi dato y me devuelve un entero
-//Se re crea un bucle para recorrer toda la palabra o frase ingresada
- for (let j = 0; j < string.length; j++) {
-  let codigoAsc = string.charCodeAt(j);// se declara la variable codigoAd donde se almacenara el codigo ASCII de la primera letra de la palabra
-    if (codigoAsc >= 65 && codigoAsc<=90) {//codicionamos delimitando el caracter que son MAYUSCULAS
-       caracterAc = ((codigoAsc-90-offset)%26 + 90); //formula para realizar el desplazamiento fijo que se da, obteniendo el nuevo caracter
-    }else if(codigoAsc >= 97 && codigoAsc<=122)  {//codicionamos delimitando el caracter que son minusculas
-           caracterAc = ((codigoAsc-122-offset)%26 + 122); //formula para realizar el desplazamiento fijo que se da, obteniendo el nuevo caracter
-          }else{
-            caracterAc = codigoAsc;//se obtiene el mismo valor
-          }
-    caracterD = String.fromCharCode(caracterAc);// registra el cambio del codigo ASCII a la palabra correcta
-    desencryptWord += caracterD;//el contenedor almacena cada caracter
-  }
-  return desencryptWord;
-},
-createCipherWithOffset: (offset)=>  {
-let cipherED = {
-  encode:(string)=>  {
-    return cipher.encode(offset,string);
+  encode: (offset, string) => {
+    //se declaran las variables que van a contener ciertas valores
+    let caracterActual = "";
+    let caracterNuevo = " ";
+    let encryptWord = "";
+    // let desplazarN= parseInt(offset);//parsea mi dato y me devuelve un entero
+    //Se re crea un bucle para recorrer toda la palabra o frase ingresada
+    for (let i = 0; i < string.length; i++) {
+      let codigoAsCii = string.charCodeAt(i);
+      if (codigoAsCii >= 65 && codigoAsCii <= 90) {
+        caracterActual = ((codigoAsCii - 65 + offset) % 26 + 65);
+      } else if (codigoAsCii >= 97 && codigoAsCii <= 122) {
+        caracterActual = ((codigoAsCii - 97 + offset) % 26 + 97);
+      } else {
+        caracterActual = codigoAsCii;
+      }
+      caracterNuevo = String.fromCharCode(caracterActual);
+      encryptWord += caracterNuevo;
+    }
+    return encryptWord;
   },
-  decode:(string)=>  {
-    return cipher.decode(offset,string);
+  // La funcion cifra la palabra la inresar
+  decode: (offset, string) => {
+    //se declaran las variables que van a contener ciertas valores
+    let caracterActual = "";
+    let caracterNuevo = "";
+    let desencryptWord = "";
+    // let desplazarN= parseInt(offset);//parsea mi dato y me devuelve un entero
+    //Se re crea un bucle para recorrer toda la palabra o frase ingresada
+    for (let j = 0; j < string.length; j++) {
+      let codigoAsCii = string.charCodeAt(j);
+      if (codigoAsCii >= 65 && codigoAsCii <= 90) {
+        caracterActual = ((codigoAsCii - 90 - offset) % 26 + 90);
+      } else if (codigoAsCii >= 97 && codigoAsCii <= 122) {
+        caracterActual = ((codigoAsCii - 122 - offset) % 26 + 122);
+      } else {
+        caracterAc = codigoAsc;
+      }
+      caracterNuevo = String.fromCharCode(caracterActual);
+      desencryptWord += caracterNuevo;
+    }
+    return desencryptWord;
+  },
+  createCipherWithOffset: (offset) => {
+    let cipherED = {
+      encode: (string) => {
+        return cipher.encode(offset, string);
+      },
+      decode: (string) => {
+        return cipher.decode(offset, string);
+      }
+    };
+    return cipherED;
   }
-};
-return cipherED;
-}
 };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 // se declara variables para capturar los ids del html y los datos que se incluyan
-let mensajeE = document.getElementById("textI");
-let mensajeS= document.getElementById("textO"); // se declara la vaariable donde se obtendra el contenido donde se ingreso el id
-let numb=document.getElementById("camb");
+let mensajeEntrada = document.getElementById("textI");
+let mensajeSalida= document.getElementById("textO"); 
+let numeroDesplazamiento=document.getElementById("camb");
 //se declara las variables para capturar los ids de los buttons 
-let btnC= document.getElementById("cipherB");
-let btnD= document.getElementById("decipherB");
-let btnclean=document.getElementById("resetT");
+let buttonCifrar= document.getElementById("cipherB");
+let buttonDescifrar= document.getElementById("decipherB");
+let buttonclean=document.getElementById("resetT");
 
 //se crean dos eventos button donde se pasan las funciones del cifrado y descifrado, 
-btnC.addEventListener('click',()=>{
- 
-mensajeS.value=cipher.encode(parseInt(numb.value),mensajeE.value);
+buttonCifrar.addEventListener('click',()=>{
+  mensajeSalida.value=cipher.encode(parseInt(numeroDesplazamiento.value),mensajeEntrada.value);
 });
  
-btnD.addEventListener('click',()=>{
-  mensajeS.value=cipher.decode(parseInt(numb.value),mensajeE.value);
+buttonDescifrar.addEventListener('click',()=>{
+  mensajeSalida.value=cipher.decode(parseInt(numeroDesplazamiento.value),mensajeEntrada.value);
 });
-btnclean.addEventListener('click',()=>{
-  mensajeS.value = " ";
-  mensajeE.value = " ";
-  numb.value = " ";
+
+buttonclean.addEventListener('click',()=>{
+  mensajeSalida.value = " ";
+  mensajeEntrada.value = " ";
+  numeroDesplazamiento.value = " ";
 });
